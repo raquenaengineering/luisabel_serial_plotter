7692df43158393f18a6f37c8d6964bdfae17e9a0
Raquenaengineering <Raquenaengineering@gmail.com>
2020-12-24 01:35:39 +0100

    - Add menu bar - add grid to plot - limit plot dimensions - set default plot range - add action to send button. - add action to enter on textbox.

--- arduino_plotter_pyqt.py
+++ arduino_plotter_pyqt.py
@@ -21,6 +21,7 @@ from PyQt5.QtWidgets import (
 	QPushButton,
 	QMenuBar,
 	QToolBar,
+	QStatusBar,
 
 	QSystemTrayIcon,
 	QTextEdit,
@@ -34,7 +35,8 @@ from PyQt5.QtGui import (
 )
 
 from PyQt5.QtCore import(
-	Qt
+	Qt,
+	QSize
 )
 
 import pyqtgraph as pg
@@ -119,13 +121,26 @@ class MyGraph(pg.PlotWidget):
 	def __init__(self):
 		super().__init__()			
 		self.setBackground([200,200,200])								# changing default background color.
+		self.showGrid(x = True, y = True, alpha = 0.5)
 		# do something to set the default axes range
+		self.setRange(xRange = [0,1000], yRange = [-200,200])
+		self.setLimits(xMin=0, xMax=1000000, yMin=-1000, yMax=1000)		# THIS MAY ENTER IN CONFIG WITH PLOTTING !!!
+	
+		
+class QPaletteButton(QPushButton):
+	def __init__(self,color):							# color as input parameter
+		super().__init__()
+		self.setFixedSize(QSize(24,24))
+		self.color = color
+		self.setStyleSheet("background-color: " + color)				# the book uses c-ish like syntax. 
+					
 
 class MainWindow(QMainWindow): 
 	
 	# class variables #
 	serial_ports = list													# list of serial ports detected, probably this is better somewhere else !!!
 	
+	# constructor # 
 	def __init__(self):
 		super().__init__()
 		# window stuff #
@@ -135,24 +150,17 @@ class MainWindow(QMainWindow):
 		# menubar #
 		menu = self.menuBar()											# by default, the window already has an instance/object menubar
 		# file #
-		file_menu = menu.addMenu("&File")
-		serial_port_menu = file_menu.addMenu("Serial Port")
-		# here we need to add an entry for each serial port avaiable at the computer
-		# 1. How to get the list of available serial ports ?
-		self.get_serial_ports()											# meeded to list the serial ports at the menu
-		# 2. How to update the list of available serial ports ?
-		self.get_serial_ports()											# calling this function will update the list (use a timer???, thread???)
-		# 3. How to ensure which serial ports are available ? (grey out the unusable ones)
-		# 4. How to display properly each available serial port at the menu ?
-		for port in self.serial_ports:
-			serial_port_menu.addAction(port[0])
+		self.file_menu = menu.addMenu("&File")
+		self.serial_port_menu = self.file_menu.addMenu("Serial Port")
+		self.update_serial_ports()
+
 		# about #
 		help_menu = menu.addMenu("&Help")
 		about_menu = help_menu.addAction("About")
 		
 		
 		button_action = QAction()
-		file_menu.addAction(button_action)
+		self.file_menu.addAction(button_action)
 		
 		#self.toolbar.setIconSize(Qsize(16,16))
 		#self.addToolBar(self.toolbar)
@@ -173,22 +181,46 @@ class MainWindow(QMainWindow):
 		self.combo_serial_speed = QComboBox()
 		self.combo_serial_speed.setEditable(False)						# by default it isn't editable, but just in case.
 		self.combo_serial_speed.addItems(SERIAL_SPEEDS)
+		self.combo_serial_speed.currentTextChanged.connect(				# on change on the serial speed textbox, we call the connected mthod
+			self.change_serial_speed) 									# we'll figure out which is the serial speed at the method (would be possible to use a lambda?) 
 		self.layoutH1.addWidget(self.combo_serial_speed)				# 
 		self.label_baud = QLabel("baud")
 		self.layoutH1.addWidget(self.label_baud)
 		self.textbox_send_command = QLineEdit()
+		self.textbox_send_command.returnPressed.connect(self.send_serial)			# sends command via serial port
 		self.layoutH1.addWidget(self.textbox_send_command)
-		self.b_send = QPushButton("Send")
+		self.b_send = QPushButton("Send")											
+		self.b_send.clicked.connect(self.send_serial)								# same action as enter in textbox
 		self.layoutH1.addWidget(self.b_send)
 		self.combo_endline_params = QComboBox()
 		self.combo_endline_params.addItems(ENDLINE_OPTIONS)
+		self.combo_endline_params.currentTextChanged.connect(self.change_endline_style)
 		self.layoutH1.addWidget(self.combo_endline_params)
+		# ~ self.layoutH2 = QHBoxLayout()
+		# ~ self.layoutV1.addLayout(self.layoutH2)
+		# ~ self.add_palette_buttons(self.layoutH2)
 		
+		# status bar #
+		self.status_bar = QStatusBar()
+		self.setStatusBar(self.status_bar)
+		self.status_bar.showMessage("Not Connected")
+		# 3. write text saying no serial port is connected
 
 		# show and done #
 		self.show()
 		
 		
+		# actions #		
+		
+	def send_serial(self):
+		print("Send Serial")
+		self.textbox_send_command.setText("")
+		#2.delete textbox content. 
+		#self.centralWidget().setText("BOOM!")
+		
+		# here the serial send command # 
+	
+	# other methods # 
 		
 	def get_serial_ports(self):
 				
@@ -211,31 +243,64 @@ class MainWindow(QMainWindow):
 		logging.debug(port_descs)
 		logging.debug("---------------------------------------------------")
 		
-	def print_serial_ports(self):			# delete, just for testing !!!
-		print(self.serial_ports)
-
-
-
-	# ~ # creates a full palete with as many buttons as colors as defined in COLORS
-	# ~ def add_palette_buttons(self,layout):
-		# ~ for c in COLORS:
-			# ~ b = QPaletteButton(c)
-			# ~ b.pressed.connect(lambda c = c: self.canvas.set_pen_color(c))	# not completely sure about how lambda functions work
-			# ~ layout.addWidget(b)
-
-
-		# ~ self.counter = 0
-		# ~ self.l = QLabel("Start")
-		# ~ b = QPushButton("DANGER!")
-		# ~ b.pressed.connect(self.oh_no)
-		# ~ self.layout.addWidget(self.l)
-		# ~ self.layout.addWidget(b)
-		
-		
 
 		
+	def change_serial_speed(self):
+		print("change_serial_speed method called")
+		text_val = self.combo_serial_speed.currentText()
+		print(text_val)
+		
+	def change_endline_style(self):										# this and previous method are the same, use lambdas?
+		endline_style = self.combo_endline_params.currentText()
+		print(endline_style)
+
+		
+	def select_serial_port(self):
+		serial_port_name = 	"s"
+		print(serial_port_name)
+		
+		
+		
+	def on_port_select(self):				# callback when COM port is selected at the menu.
+		#1. get the selected port name via the text. 
+		#2. open a serial communication.
+		pass
+		
+	def serial_connect(self, port_name):
+		print("serial_connect method called")
+		print("port name " + port_name)
 
 
+	# creates a full palete with as many buttons as colors as defined in COLORS
+	def add_palette_buttons(self,layout):
+		for c in COLORS:
+			b = QPaletteButton(c)
+			b.pressed.connect(lambda c = c: self.canvas.set_pen_color(c))	# not completely sure about how lambda functions work
+			layout.addWidget(b)
+
+
+	def update_serial_ports(self):										# we update the list every time we go over the list of serial ports.
+		# here we need to add an entry for each serial port avaiable at the computer
+		# 1. How to get the list of available serial ports ?
+		self.get_serial_ports()											# meeded to list the serial ports at the menu
+		# 3. How to ensure which serial ports are available ? (grey out the unusable ones)
+		# 4. How to display properly each available serial port at the menu ?
+		print (self.serial_ports)
+		if self.serial_ports != []:
+			for port in self.serial_ports:
+				port_name = port[0]
+				print(port_name)
+				b = self.serial_port_menu.addAction(port_name)
+				b.triggered.connect((lambda: self.serial_connect(port_name)))				# just need to add somehow the serial port name here, and we're done.
+
+
+				# here we need to add the connect method to the action click, and its result
+				
+		else:
+				self.noserials = serial_port_menu.addAction("No serial Ports detected")
+				self.noserials.setDisabled(True)
+				 	
+		
 app = QApplication(sys.argv)
 mw = MainWindow()
 app.exec_()
