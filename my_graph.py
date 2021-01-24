f5d8a8cca805e0dc206efc8429f6f82a7b827aae
Raquenaengineering <Raquenaengineering@gmail.com>
2021-01-24 23:42:16 +0100

    - Add several methods to access toggle's internal variables. - Add several methods to enable/disable toggles in blocks. - remove unused variables on MyPlot.

--- my_graph.py
+++ my_graph.py
@@ -45,33 +45,26 @@ class LabelledAnimatedToggle(QWidget):
 		
 	def setLabel(self,label_text):
 		self.label.setText(label_text)	
+	def getLabel(self):
+		label = self.label.text()
+		return(label)
+		
 	def setChecked(self, val):
 		self.toggle.setChecked(val)
+	def isChecked(self):
+		return(self.toggle.isChecked())
+		
 	def setEnabled(self, val):
 		self.toggle.setEnabled(val)
+	def isEnabled(self):
+		return(self.toggle.isEnabled())
 		
-
 class MyPlot(QWidget):
-	
-	# Arduino serial plotter has 500 points max. on the x axis.
-	max_points = None													# maximum points per plot
-	
-	tvec = []															# independent variable, with "max_points" points. 
-			
+					
 	n_plots = 12														# number of plots on the current plot. 
-	first = True														# first iteration only creating the plots
 	plot_tick_ms = 20													# every "plot_tick_ms", the plot updates, no matter if there's new data or not. 
-	
-	
 	dataset = []														# complete dataset, this should go to a file.							
-	plot_refs = []														# references to the different added plots.
-	toggles = []														# references to the toggles which enable/disable plots.
-	enabled_graphs = []
-	names_refs = []														# references with the names
-	plot_subset = []
-													
-	dataset_changed = False	
-	
+	toggles = []														# references to the toggles which enable/disable plots.													
 	
 	def __init__(self, dataset = [], max_points = 200):
 		super().__init__()	
@@ -92,34 +85,65 @@ class MyPlot(QWidget):
 		self.plot_timer = QTimer()										# used to update the plot
 		self.plot_timer.timeout.connect(self.on_plot_timer)				# 
 		self.start_plotting(self.plot_tick_ms)
-		self.stop_plotting()			
+		self.stop_plotting()	
+
+		self.set_enabled_graphs("none")									# writes to a variable of graph indicating which graphs are on
 		
-	def set_channels_labels(self,names):
+		print("Init until set_enabled_graphs")
+		
+		for toggle in self.toggles:
+			toggle.setChecked(True)
+			toggle.setEnabled(True)
+		
+		for toggle in self.toggles:
+			print("IsChecked")
+			print(toggle.isChecked())
+			print("IsEnabled")
+			print(toggle.isEnabled())
+						
+	def add_toggles(self):												# encapsulates the creation of the toggles, and their initial setup.
+		for i in range(0, MAX_PLOTS):
+			color = "#"+COLORS[i]
+			label_toggle = LabelledAnimatedToggle(color = color)
+			self.toggles.append(label_toggle)
+			label_toggle.setChecked(False)						# all toggles not checked by default	# create new method to call the toggle method?
+			label_toggle.setEnabled(True)						# all toggles not enabled by default
+			self.layout_channel_select.addWidget(label_toggle)
+			
+	def enable_toggles(self,val):
+		if(val == "all"):
+			for i in range(MAX_PLOTS):
+				self.toggles.setEnabled(True)
+		elif(val == "none"):
+			for i in range(MAX_PLOTS):
+				self.toggles.setEnabled(False) 
+		else:
+			pass			# fill with behavior if val is a vector
+		
+	def check_toggles(self,val):
+		if(val == "all"):
+			for i in range(MAX_PLOTS):
+				self.toggles.setChecked(True)
+		elif(val == "none"):
+			for i in range(MAX_PLOTS):
+				self.toggles.setChecked(False) 
+		else:
+			pass
+		
+	def set_channels_labels(self,names):								# each channel toggle has a label, set the text on that label.
 		for i in range(MAX_PLOTS):										# we only assign the names of the plots that can be plotted
 			try:
 				self.toggles[i].setLabel(names[i])
 			except Exception as e:
 				logging.debug("more channels than labels")
-				#print(e)		
-
-	def clear_channels_labels(self):
+				
+	def clear_channels_labels(self):									# clear all labels, usually to set them with new vals.
 		for i in range(MAX_PLOTS):										# we only assign the names of the plots that can be plotted
 			try:
 				self.toggles[i].setLabel('')
 			except Exception as e:
 				logging.debug("more channels than labels")
-				#print(e)	
 					
-	def add_toggles(self):
-		for i in range(0, MAX_PLOTS):
-			color = "#"+COLORS[i]
-			#print(color)			
-			#label_toggle = LabelledAnimatedToggle()
-			label_toggle = LabelledAnimatedToggle(color = color)
-			self.toggles.append(label_toggle)
-			label_toggle.setChecked(True)						# all toggles not checked by default	# create new method to call the toggle method?
-			label_toggle.setEnabled(True)						# all toggles not enabled by default
-			self.layout_channel_select.addWidget(label_toggle)
 
 	def create_plots(self):
 		self.graph.create_plots()
@@ -129,14 +153,22 @@ class MyPlot(QWidget):
 
 	def on_plot_timer(self):											# this is an option, to add together toggle processing and replot.
 		#print("PLOT TIMER MyPlot")
-		self.enabled_graphs = []
+		# ~ self.enabled_graphs = []
+		# ~ for i in range(0,MAX_PLOTS):
+			# ~ if(self.toggles[i].toggle.isChecked()):
+				# ~ self.enabled_graphs.append(True)
+			# ~ else:
+				# ~ self.enabled_graphs.append(False)
+		enabled = []
 		for i in range(0,MAX_PLOTS):
 			if(self.toggles[i].toggle.isChecked()):
-				self.enabled_graphs.append(True)
+				enabled.append(True)
 			else:
-				self.enabled_graphs.append(False)
+				enabled.append(False)		
 			
-		self.graph.enable_graphs(self.enabled_graphs)					# writes to a variable of graph indicating which graphs are on
+		self.set_enabled_graphs(enabled)
+				
+			
 		self.graph.on_plot_timer()										# calls the regular plot timer from graph.
 		
 	def plot_timer_start(self):											
@@ -157,6 +189,20 @@ class MyPlot(QWidget):
 	def stop_plotting(self):
 		self.plot_timer.stop()
 
+	def set_enabled_graphs(self, enable_list):
+		if enable_list == "all":
+			enable_list = []
+			for i in range(MAX_PLOTS):
+				enable_list.append(True)
+		elif enable_list == "none":
+			enable_list = []
+			for i in range(MAX_PLOTS):
+				enable_list.append(False)
+		
+		self.graph.set_enabled_graphs(enable_list)
+						
+
+
 class MyGraph(pg.PlotWidget):											# this is supposed to be the python convention for classes. 
 	
 	# Arduino serial plotter has 500 points max. on the x axis.
@@ -170,10 +216,10 @@ class MyGraph(pg.PlotWidget):											# this is supposed to be the python conv
 	dataset = None														# complete dataset, this should go to a file.							
 	plot_refs = []														# references to the different added plots.
 	plot_subset = []
-	active_graphs = []
+	enabled_graphs = []													# enabled graphs ON GRAPH WINDOW, not on toggles. 
 	
 	for i in range(0,MAX_PLOTS):
-		active_graphs.append(False)
+		enabled_graphs.append(False)									
 													
 	dataset_changed = False
 
@@ -195,7 +241,7 @@ class MyGraph(pg.PlotWidget):											# this is supposed to be the python conv
 		self.setBackground([70,70,70])									# changing default background color.
 		self.showGrid(x = True, y = True, alpha = 0.5)
 		self.setRange(xRange = [0,self.max_points], yRange = [-1200,1200]) # set default axes range
-		self.setLimits(xMin=0, xMax=self.max_points, yMin=-1000, yMax=1000)	# THIS MAY ENTER IN CONFIG WITH PLOTTING !!!
+		self.setLimits(xMin=0, xMax=self.max_points, yMin=-1000, yMax=10000)	# THIS MAY ENTER IN CONFIG WITH PLOTTING !!!
 		#self.enableAutoRange(axis='x', enable=True)						# enabling autorange for x axis
 		legend = self.addLegend()
 		self.setTitle(title)											# if title is wanted								
@@ -208,13 +254,16 @@ class MyGraph(pg.PlotWidget):											# this is supposed to be the python conv
 
 			self.first = False
 
-	def clear_plot(self):												# NOT WORKING 
+	def clear_plot(self):												
 		print("clear_plot method called")
 		for i in range(len(self.plot_subset)):
 			self.plot_refs[i].clear()									# clears the plot
 			self.plot_refs[i].setData([0])								# sets the data to 0, may not be necessary
 			#self.plot_subset[i] = []
 			
+	def set_enabled_graphs(self,enabled_graphs):						# enabled/dsables graphs ON GRAPH WINDOW, not on the toggles.
+		self.enabled_graphs = enabled_graphs
+
 	
 	def on_plot_timer(self):
 		#print("PLOT_TIMER MyGraph")										
@@ -232,8 +281,8 @@ class MyGraph(pg.PlotWidget):											# this is supposed to be the python conv
 			#print(len(self.plot_subset))
 			self.dataset_changed = False
 			for i in range(len(self.plot_subset)):
-				#print(self.active_graphs[i])
-				if(self.active_graphs[i] == True):
+				print(self.enabled_graphs[i])
+				if(self.enabled_graphs[i] == True):
 					self.plot_refs[i].setData(self.plot_subset[i], name = "small penis") 		# required for update: reassign references to the plots
 					# self.plot_refs[i].setData(self.t, self.plot_subset[i])# required for update: reassign references to the plots
 				else:
@@ -246,11 +295,6 @@ class MyGraph(pg.PlotWidget):											# this is supposed to be the python conv
 			pg.QtGui.QApplication.processEvents()						# for whatever reason, works faster when using processEvent.
 		
 
-	def enable_graphs(self, enable_list):	
-		self.active_graphs = enable_list
-		#for i in range(0,MAX_PLOTS):
-						
-
 ## THIS PART WON'T BE EXECUTED WHEN IMPORTED AS A SUBMODULE, BUT ONLY WHEN TESTED INDEPENDENTLY ##
 
 if __name__ == "__main__":
@@ -258,7 +302,7 @@ if __name__ == "__main__":
 	class MainWindow(QMainWindow):
 		
 		# class variables #
-		data_tick_ms = 20
+		data_tick_ms = 500
 
 		#creating a fixed size dataset #
 		dataset = []
@@ -295,6 +339,35 @@ if __name__ == "__main__":
 			self.data_timer.timeout.connect(self.on_data_timer)
 			self.data_timer.start(self.data_tick_ms)
 
+			self.plot.check_toggles([
+			True,
+			True,
+			True,
+			True,
+			True,
+			False,
+			False,
+			False,
+			False,
+			False,
+			False,
+			])
+
+			self.plot.enable_toggles([
+			True,
+			True,
+			True,
+			True,
+			True,
+			False,
+			False,
+			False,
+			False,
+			False,
+			False,
+			])
+								
+
 
 			self.setCentralWidget(self.plot)
 			# last step is showing the window #
@@ -302,9 +375,7 @@ if __name__ == "__main__":
 			
 			#self.plot.graph.plot_timer.start()
 			
-			
-
-			
+		
 		def on_data_timer(self):										# simulate data coming from external source at regular rate.
 			t0 = time.time()
 			logging.debug("length of dataset: " + str(len(self.plot.dataset)))
