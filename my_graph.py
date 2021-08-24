7e84dc142a9511e527271f7d2d9569508351b81b
Raquenaengineering <Raquenaengineering.com>
2021-08-24 10:25:47 +0200

    - Minor changes

--- my_graph.py
+++ my_graph.py
@@ -29,8 +29,8 @@ COLORS = ["ff0000","00ff00","0000ff","ffff00","ff00ff","00ffff",
 			"FFA500","7fff00","00ff7f","007FFF","EE82EE","FF007F",]
 
 MAX_PLOTS = 12															# Absolute maximum number of plots, change if needed !!
-DEFAULT_Y_MAX = 300
-DEFAULT_Y_MIN = 0
+DEFAULT_Y_MAX = 35000
+DEFAULT_Y_MIN = -35000
 DEFAULT_MAX_POINTS = 2000
 CHANNEL_LABEL_MAX_LEN = 10
 		
